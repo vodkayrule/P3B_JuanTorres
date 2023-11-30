f0bce30acb74b20e4b78683081d46e2e7ed056eb
vodkayrule <151049925+vodkayrule@users.noreply.github.com>
2023-11-30 16:15:47 +0100

    Update Clase.java

--- Proyecto3B/src/Clase.java
+++ Proyecto3B/src/Clase.java
@@ -4,6 +4,7 @@ public class Clase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Juan Torres Calero");
+		System.out.println("Estoy llegando al final");
 	}
 
 }
